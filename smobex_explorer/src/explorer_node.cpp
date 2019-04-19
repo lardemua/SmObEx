572e1a4543d9beb688309c63405d2d6a3702eb17
Joao <joao.pm.santos96@hotmail.com>
2019-04-19 17:55:08 +0100

    move_group implementations

--- smobex_explorer/src/explorer_node.cpp
+++ smobex_explorer/src/explorer_node.cpp
@@ -1,14 +1,10 @@
-#include <geometry_msgs/PoseArray.h>
-#include <visualization_msgs/MarkerArray.h>
-
-#include <cmath>
-#include <math.h>
 #include <ros/ros.h>
 
-#include <pcl_ros/point_cloud.h>
-#include <pcl/point_types.h>
+#include <moveit/move_group_interface/move_group_interface.h>
+#include <moveit/planning_scene_interface/planning_scene_interface.h>
 
-#include <smobex_explorer/explorer.h>
+#include <moveit_msgs/DisplayRobotState.h>
+#include <moveit_msgs/DisplayTrajectory.h>
 
 using namespace std;
 
@@ -16,109 +12,61 @@ int main(int argc, char **argv)
 {
     ros::init(argc, argv, "explorer_node");
 
-    ros::NodeHandle n;
+    ros::AsyncSpinner spinner(1);
+    spinner.start();
 
-    ros::Publisher pub_poseArray = n.advertise<geometry_msgs::PoseArray>("/pose_array", 10);
-    ros::Publisher pub_lines = n.advertise<visualization_msgs::MarkerArray>("/ray_cast_lines", 10);
+    static const std::string PLANNING_GROUP = "manipulator";
 
-    ros::Publisher pub_blue = n.advertise<visualization_msgs::MarkerArray>("/discovered_space", 10);
-    ros::Publisher pub_text = n.advertise<visualization_msgs::MarkerArray>("/pose_text", 10);
+    moveit::planning_interface::MoveGroupInterface move_group(PLANNING_GROUP);
 
-    // ros::Subscriber sub = n.subscribe("/octomap_full", 1, octomapCallback);
+    moveit::planning_interface::PlanningSceneInterface planning_scene_interface;
 
-    // ros::Subscriber sub_spaceDefined = n.subscribe<visualization_msgs::MarkerArray>("/cells_vis_array", 10, mapCallback);
-    // ros::Subscriber octomap_sub = n.subscribe("octomap_full", 10, &evaluatePose::writeOctomap, &poseee);
+    const robot_state::JointModelGroup *joint_model_group = move_group.getCurrentState()->getJointModelGroup(PLANNING_GROUP);
 
-    ros::Rate loop_rate(10);
+    ROS_INFO_NAMED("tutorial", "Reference frame: %s", move_group.getPlanningFrame().c_str());
+    ROS_INFO_NAMED("tutorial", "End effector link: %s", move_group.getEndEffectorLink().c_str());
 
-    srand(time(NULL));
+#if 1
 
-    pcl::PointCloud<pcl::PointXYZ> center_points;
+    geometry_msgs::Pose target_pose1;
+    target_pose1.orientation.w = 1.0;
+    target_pose1.position.x = 0.6;
+    target_pose1.position.y = 0.3;
+    target_pose1.position.z = 0.6;
+    move_group.setPoseTarget(target_pose1);
 
-    center_points.push_back(pcl::PointXYZ(1.5, 0, 0.5));
+    moveit::planning_interface::MoveGroupInterface::Plan my_plan;
 
-    std::string fixed_frame = "/map";
-    float r_min = 0, r_max = 1;
-    int n_poses = 20;
+    bool success = (move_group.plan(my_plan) == moveit::planning_interface::MoveItErrorCode::SUCCESS);
 
-    ros::param::get("~fixed_frame", fixed_frame);
-    ros::param::get("~r_min", r_min);
-    ros::param::get("~r_max", r_max);
-    ros::param::get("~n_poses", n_poses);
+    ROS_INFO_NAMED("tutorial", "Visualizing plan 1 (pose goal) %s", success ? "" : "FAILED");
 
-    ros::Time t = ros::Time::now();
+#else
 
-    geometry_msgs::PoseArray point_poses;
+    moveit::planning_interface::MoveGroupInterface::Plan my_plan;
 
-    point_poses.header.frame_id = fixed_frame;
-    point_poses.header.stamp = t;
+    moveit::core::RobotStatePtr current_state = move_group.getCurrentState();
 
-    int num_of_points = center_points.size();
+    std::vector<double> joint_group_positions;
+    current_state->copyJointGroupPositions(joint_model_group, joint_group_positions);
 
-    std::vector<evaluatePose> poses(n_poses, evaluatePose(20, 0.8, 10, 58 * M_PI / 180, 45 * M_PI / 180));
+    joint_group_positions[0] = 0.0; // radians
+    joint_group_positions[1] = 0.0; // radians
+    joint_group_positions[2] = 0.0; // radians
+    joint_group_positions[3] = 0.0; // radians
+    joint_group_positions[4] = 0.0; // radians
+    joint_group_positions[5] = 0.0; // radians
+    move_group.setJointValueTarget(joint_group_positions);
 
-    int k = 0;
+    bool success = (move_group.plan(my_plan) == moveit::planning_interface::MoveItErrorCode::SUCCESS);
+    ROS_INFO_NAMED("tutorial", "Visualizing plan 2 (joint space goal) %s", success ? "" : "FAILED");
 
-    // visualization_msgs::Marker text;
-    visualization_msgs::MarkerArray found_space, text_pose, line_vis;
+#endif
 
-    for (int i = 0; i < num_of_points; i++)
-    {
-        for (int n = 0; n < n_poses; n++)
-        {
-            pcl::PointXYZ pcl_point = center_points.at(i);
-            tf::Point tf_point;
-            tf::Pose tf_pose;
-            geometry_msgs::Pose one_pose;
+    /* Uncomment below line when working with a real robot */
+    move_group.move();
 
-            tf_point.setX(pcl_point.x);
-            tf_point.setY(pcl_point.y);
-            tf_point.setZ(pcl_point.z);
-
-            // tf_pose = genPose(r_min, r_max, tf_point);
-            poses[k].genPose(r_min, r_max, tf_point);
-
-            tf_pose = poses[k].view_pose;
-
-            tf::poseTFToMsg(tf_pose, one_pose);
-
-            point_poses.poses.push_back(one_pose);
-
-            text_pose.markers.push_back(poses[k].textVis(fixed_frame));
-
-            visualization_msgs::Marker line;
-            line = poses[k].rayLinesVis(fixed_frame);
-            // line = poses[k].frustumLinesVis(fixed_frame, k);
-
-            line_vis.markers.push_back(line);
-
-            visualization_msgs::MarkerArray single_view_boxes;
-            single_view_boxes = poses[k].discoveredBoxesVis(fixed_frame);
-
-            for (size_t i = 0; i < single_view_boxes.markers.size(); i++)
-            {
-                found_space.markers.push_back(single_view_boxes.markers[i]);
-            }
-
-            k++;
-            // ROS_INFO("Pose published: %d", n);
-        }
-    }
-
-    // line_vis = poses[0].rayLinesVis(fixed_frame);
-    // found_space = poses[0].discoveredBoxesVis(fixed_frame, 0);
-
-    while (ros::ok())
-    {
-        pub_poseArray.publish(point_poses);
-        pub_lines.publish(line_vis);
-        pub_blue.publish(found_space);
-        pub_text.publish(text_pose);
-
-        ros::spinOnce();
-
-        loop_rate.sleep();
-    }
+    ros::shutdown();
 
     return 0;
 }
