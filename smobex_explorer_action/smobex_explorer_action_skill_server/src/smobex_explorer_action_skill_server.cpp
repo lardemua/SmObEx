6a1d8f2f883eb8ce3504b18e6be6ac83d2d1e4c4
Joao <joao.pm.santos96@hotmail.com>
2019-05-15 14:11:38 +0100

    Fixed missing dependencies

--- smobex_explorer_action/smobex_explorer_action_skill_server/src/smobex_explorer_action_skill_server.cpp
+++ smobex_explorer_action/smobex_explorer_action_skill_server/src/smobex_explorer_action_skill_server.cpp
@@ -2,6 +2,38 @@
 #include <actionlib/server/simple_action_server.h>
 #include <smobex_explorer_action_skill_server/smobex_explorer_action_skill_server.h>
 
+#include <moveit/move_group_interface/move_group_interface.h>
+#include <moveit/planning_scene_interface/planning_scene_interface.h>
+#include <moveit_msgs/DisplayRobotState.h>
+#include <moveit_msgs/DisplayTrajectory.h>
+
+#include <moveit/trajectory_processing/iterative_time_parameterization.h>
+#include <moveit/robot_trajectory/robot_trajectory.h>
+
+#include <tf/LinearMath/Matrix3x3.h>
+#include <tf/LinearMath/Quaternion.h>
+#include <tf/LinearMath/Vector3.h>
+#include <tf/tf.h>
+#include <tf/transform_datatypes.h>
+
+#include <sensor_msgs/PointCloud2.h>
+#include <visualization_msgs/Marker.h>
+#include <visualization_msgs/MarkerArray.h>
+
+#include <pcl/point_types.h>
+#include <pcl_ros/point_cloud.h>
+#include <pcl_ros/transforms.h>
+
+#include <pcl/console/time.h>
+#include <pcl/io/pcd_io.h>
+#include <pcl/point_types.h>
+
+#include <pcl/features/normal_3d.h>
+#include <pcl/filters/voxel_grid.h>
+#include <pcl/segmentation/conditional_euclidean_clustering.h>
+
+#include <smobex_explorer/explorer.h>
+
 SmobexExplorerActionSkill::SmobexExplorerActionSkill(std::string name) : as_(nh_, name, boost::bind(&SmobexExplorerActionSkill::executeCB, this, _1), false),
                                                                          action_name_(name)
 {
@@ -15,6 +47,7 @@ SmobexExplorerActionSkill::~SmobexExplorerActionSkill()
 void SmobexExplorerActionSkill::executeCB(const smobex_explorer_action_skill_msgs::SmobexExplorerActionSkillGoalConstPtr &goal)
 {
   ROS_INFO_STREAM("Threshold: " << goal->threshold);
+  this->set_succeeded();
 }
 
 void SmobexExplorerActionSkill::set_succeeded(std::string outcome)
