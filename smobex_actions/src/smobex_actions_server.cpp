4e8aead736c3e5a15aac4b2eb52594f40c6f7f8c
joao-pm-santos96 <joao.pm.santos96@gmail.com>
2020-03-24 21:59:05 +0000

    Begin of application, not compiling

--- smobex_actions/src/smobex_actions_server.cpp
+++ smobex_actions/src/smobex_actions_server.cpp
@@ -1,7 +1,10 @@
 #include <ros/ros.h>
 #include <actionlib/server/simple_action_server.h>
-#include <smobex_actions/smobex_explorerAction.h>
 
+#include <moveit/move_group_interface/move_group_interface.h>
+#include <moveit/planning_scene_interface/planning_scene_interface.h>
+
+#include <smobex_actions/smobex_explorerAction.h>
 #include <smobex_actions/smobex.h>
 
 class SmobexExplorer
@@ -27,6 +30,44 @@ public:
 
     void executeCB(const smobex_actions::smobex_explorerGoalConstPtr &goal)
     {
+
+        static const std::string planning_group = "manipulator";
+
+        ros::Publisher pub_cloud_clusters = _nh.advertise<sensor_msgs::PointCloud2>("/clusters_cloud", 10);
+        ros::Publisher pub_centers_clusters = _nh.advertise<sensor_msgs::PointCloud2>("/clusters_centers", 10);
+        ros::Publisher pub_arrows = _nh.advertise<visualization_msgs::MarkerArray>("/pose_arrows", 10);
+        ros::Publisher pub_space = _nh.advertise<visualization_msgs::MarkerArray>("/discovered_space", 10);
+
+        moveit::planning_interface::MoveGroupInterface move_group(planning_group);
+        moveit::planning_interface::PlanningSceneInterface planning_scene_interface;
+        moveit::planning_interface::MoveGroupInterface::Plan plan;
+
+        const robot_state::JointModelGroup *joint_model_group =
+            move_group.getCurrentState()->getJointModelGroup(planning_group);
+
+        std_msgs::ColorRGBA green_color;
+        green_color.r = 0.0;
+        green_color.g = 1.0;
+        green_color.b = 0.0;
+        green_color.a = 1.0;
+
+        float min_range = 0;
+        float max_range = 1;
+        float width_FOV = M_PI;
+        float height_FOV = M_PI;
+        float resolution = 0.5;
+        std::string frame_id = "/world";
+
+        ros::param::get("~" + ros::names::remap("min_range"), min_range);
+        ros::param::get("~" + ros::names::remap("max_range"), max_range);
+        ros::param::get("~" + ros::names::remap("width_FOV"), width_FOV);
+        ros::param::get("~" + ros::names::remap("height_FOV"), height_FOV);
+        ros::param::get("~" + ros::names::remap("frame_id"), frame_id);
+        // ros::param::get("octomap_resolution", resolution);
+
+        Smobex explorer;
+
+        explorer.setValues(min_range, max_range, width_FOV, height_FOV, resolution);
 
     }
 
